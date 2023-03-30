d0100beff140c4d0364012fb2a050a6c6cadcea5
MohonSaha <practicewithmohon@gmail.com>
2023-03-30 23:18:55 +0600

    add navbar

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Home = () => {
+
+
+
+
+
+
+    return (
+        <div>
+            <div className="home-container">
+                <div className="blog-container">
+
+                </div>
+                <div className="bookmark-container">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Logo from '../../../public/logo.png'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div>
-            <h1>Hello</h1>
+            <div className='header'>
+                <img src={Logo} alt="" />
+                <div>
+                    <a href="1">Blog</a>
+                    <a href="/order">News</a>
+                    <a href="/inventory">About Us</a>
+                    <a href="/login">Login</a>
+                </div>
+            </div>
         </div>
     );
 };
